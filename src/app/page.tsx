b3bec5af95a1f2fa69bfe6458a7b63ec1ca4c166
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-03-20 14:26:02 -0300

    started user card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "./page.module.css";
+import UserCard from "@/components/userCard";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
+    <div className="max-w-5xl mx-auto">
+      <div className="bg-zinc-900 py-3 px-5 my-2 rounded">
+        <h1 className="text-2xl font-bold">Project's Components</h1>
+      </div>
+      <div className="bg-zinc-900 py-3 px-5 my-2 rounded">
+        <h2 className="text-xl font-bold border-b-2 mb-5">User Card</h2>
+        <UserCard/>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/userCard.tsx
@@ -0,0 +1,7 @@
+export default function UserCard() {
+    return (
+      <div className="bg-neutral-800 p-1">
+       lol
+      </div>
+    );
+  }
