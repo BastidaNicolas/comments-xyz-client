8dc92411944e78b561db6a690170870260e3481b
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-03-23 21:37:48 -0300

    userForm scaffold

--- src/components/userForm.tsx
+++ src/components/userForm.tsx
@@ -1,8 +1,30 @@
+'use client';
+import Image from "next/image";
+import { useState } from "react";
+
 export default function UserForm() {
+    
+    const [profileImage, setProfileImage] = useState<string>('https://i.imgur.com/fPIhmeG.jpg');
+
     return (
-        <div className="">
-            lol
-        </div>
+        <form className="">
+            <div>
+                <Image src={profileImage} alt="user profile image" width={44} height={44} ></Image>
+            </div>
+            <div>
+                <label htmlFor="usernameInput">Username</label>
+                <input type="text" name="usernameInput" id="usernameInput" />
+            </div>
+            <div>
+                <label htmlFor="imageUrlInput">Image Url</label>
+                <input type="text" name="imageUrlInput" id="imageUrlInput" value={profileImage} onChange={(e) => setProfileImage(e.target.value)}/>
+            </div>
+            <div>
+                <label htmlFor="aboutText">About</label>
+                <textarea name="aboutText" id="aboutText" cols={30} rows={10}></textarea>
+            </div>
+            <button type="submit">Save</button>
+        </form>
     );
   }
   
