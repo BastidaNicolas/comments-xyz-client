ac844cfddf3e64bd7fe4f05147d85e0a00c70db8
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-03-24 11:53:59 -0300

    userForm mockup finished

--- src/components/userForm.tsx
+++ src/components/userForm.tsx
@@ -7,23 +7,23 @@ export default function UserForm() {
     const [profileImage, setProfileImage] = useState<string>('https://i.imgur.com/fPIhmeG.jpg');
 
     return (
-        <form className="">
-            <div className="w-20 h-20">
+        <form className="max-w-sm m-auto bg-neutral-800 pt-6 px-4 pb-2 rounded-lg">
+            <div className="w-20 h-20 m-auto mb-3">
                 <Image className="rounded-full w-20 h-20 object-cover" src={profileImage} alt="user profile image" width={80} height={80} ></Image>
             </div>
-            <div>
+            <div className="flex flex-col mb-3" >
                 <label htmlFor="usernameInput">Username</label>
-                <input type="text" name="usernameInput" id="usernameInput" />
+                <input className="p-1 px-2 rounded-lg border-b border-neutral-500 bg-transparent focus:bg-neutral-700 focus:outline-none focus:text-neutral-50 text-neutral-400" type="text" name="usernameInput" id="usernameInput" />
             </div>
-            <div>
+            <div className="flex flex-col mb-3">
                 <label htmlFor="imageUrlInput">Image Url</label>
-                <input type="text" name="imageUrlInput" id="imageUrlInput" value={profileImage} onChange={(e) => setProfileImage(e.target.value)}/>
+                <input className="p-1 px-2 rounded-lg border-b border-neutral-500 bg-transparent focus:bg-neutral-700 focus:outline-none focus:text-neutral-50 text-neutral-400" type="text" name="imageUrlInput" id="imageUrlInput" value={profileImage} onChange={(e) => setProfileImage(e.target.value)}/>
             </div>
-            <div>
+            <div className="flex flex-col mb-6">
                 <label htmlFor="aboutText">About</label>
-                <textarea name="aboutText" id="aboutText" cols={30} rows={10}></textarea>
+                <textarea className="resize-none p-1 px-2 rounded-lg border-b border-neutral-500 bg-transparent focus:bg-neutral-700 focus:outline-none focus:text-neutral-50 text-neutral-400" name="aboutText" id="aboutText" cols={30} rows={10}></textarea>
             </div>
-            <button type="submit">Save</button>
+            <button type="submit" className="text-sm font-bold bg-fuchsia-600 hover:bg-fuchsia-500 hover:cursor-pointer p-1 md:px-2 md:py-1 rounded-lg peer-focus-within:self-end focus:self-end">Save</button>
         </form>
     );
   }
